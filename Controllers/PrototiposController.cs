81fc7d2b3a6e830621cd79c6dd3f2c3434700782
Oscar Cascante <cascante_98@outlook.com>
2026-06-21 19:42:36 -0600

    Prototipos Vistas

---
+++ Controllers/PrototiposController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TuProyecto.Controllers
+{
+    public class PrototiposController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Reportes()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+        public IActionResult Ventas()
+        {
+            return View();
+        }
+
+    }
+}
